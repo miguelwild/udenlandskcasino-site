5c27b170d42cd7b8f4f08ab7f52275bbd041ddda
miguelwild <dmgroupmisha93@gmail.com>
2026-01-20 20:02:35 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./", // ← ОСЬ ЦЕ КРИТИЧНО
   server: {
     host: "::",
     port: 8080,
